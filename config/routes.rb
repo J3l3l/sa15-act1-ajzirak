d0c50b6f61dff5b606a4be09926dd846878322bd
Ahmad zirak <jalal313zirak@gmail.com>
2025-04-10 00:32:21 -0500

    Final commit for SA15 - all pages and styling complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Set the homepage to your custom view
+  root "pages#home"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Routes for each custom page
+  get "/about", to: "pages#about"
+  get "/work", to: "pages#work"
+  get "/contact", to: "pages#contact"
 end
